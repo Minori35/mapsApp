85c2795c43d5d21447a028ff2fc45eaa2e68b9b9
SayraPa <98625428+Minori35@users.noreply.github.com>
2023-09-12 14:32:20 -0600

    menu de aplicacion

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface MenuItem{
+  router : string;
+  name   : string;
+}
 @Component({
-  selector: 'app-side-menu',
+  selector: 'map-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent {
 
+  public menuItems: MenuItem[]=[
+    {router: '/maps/fullscreen', name :'FullScreen'},
+    {router: '/maps/zoom-range', name :'Zoom Range'},
+    {router: '/maps/markers', name :'FullScreen'},
+    {router: '/maps/properties', name :'Properties'},
+
+  ]
 }
